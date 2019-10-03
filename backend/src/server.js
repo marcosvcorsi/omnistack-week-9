fa8216606294a7df548159dc7d3b44d176c8b89e
Marcos Corsi <marcosvcorsi@gmail.com>
2019-10-02 21:37:34 -0300

    Frontend finished and backend adjustments

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -17,7 +19,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-751xl.mongodb.net/w
 // req.params = acessa route params (para edição, delete)
 // req.body = acessa o corpo da requisição
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
