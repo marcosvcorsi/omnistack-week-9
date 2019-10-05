91c6ee5cdb2ab24a8cea70a5896529874b8faff5
Marcos Corsi <marcosvcorsi@gmail.com>
2019-10-04 21:00:49 -0300

    #omnistacknextlevel

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,35 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+const connectedUsers = {};
 
 // conectando ao mongodb
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-751xl.mongodb.net/week9?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+});
+
+// socket
+io.on('connection', socket => {
+    const {user_id} = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+})
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
 });
 
 // GET, POST, PUT, DELETE
@@ -24,4 +45,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
